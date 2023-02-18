23ade918f50ef4dd62903697f0a60851a8b51bfe
Tochi Davids <tochidavids18@gmail.com>
2023-02-18 15:48:23 +0000

    quiz jsx done + most styling done

--- quiz/src/index.js
+++ quiz/src/index.js
@@ -1,9 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/style.css";
+import "./styles/styles.css";
+import question_svg from "./media/undraw_adventure_4hum 1.svg";
+
+function App() {
+	const handleSubmit = event => {
+		event.preventDefault();
+	};
+
+	return (
+		<section className="quiz">
+			<h1>Country Quiz</h1>
+			<img src={question_svg} alt="" />
+			<h2>Kuala Lumpur is the capital of</h2>
+			<form onSubmit={handleSubmit} className="options">
+				<button type="submit" className="btn btn-outline-secondary">
+					<span className="letter">A</span>
+					<span className="answer">Vietnam</span>
+				</button>
+				<button type="submit" className="btn btn-outline-secondary">
+					<span className="letter">B</span>
+					<span className="answer">Malaysia</span>
+				</button>
+				<button type="submit" className="btn btn-outline-secondary">
+					<span className="letter">C</span>
+					<span className="answer">Sweden</span>
+				</button>
+				<button type="submit" className="btn btn-outline-secondary">
+					<span className="letter">D</span>
+					<span className="answer">Austria</span>
+				</button>
+			</form>
+		</section>
+	);
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render("hello world");
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
