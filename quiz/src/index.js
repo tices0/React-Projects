55a224d1ba85b1237e791abbbfe90966cd5834c8
Tochi Davids <tochidavids18@gmail.com>
2023-02-18 20:36:04 +0000

    all quiz styling done

--- quiz/src/index.js
+++ quiz/src/index.js
@@ -4,6 +4,47 @@ import "./styles/styles.css";
 import question_svg from "./media/undraw_adventure_4hum 1.svg";
 
 function App() {
+	const question = {
+		type: "capital",
+		options: {
+			A: "Vietnam",
+			B: "Malaysia",
+			C: "Sweden",
+			D: "Austria",
+		},
+		correct: function() {
+			return this.options.B;
+		},
+	};
+
+	console.log(question.correct(), "correct answer");
+
+	const Options = () => {
+		let options = [];
+		for (const key in question) {
+            let option
+            if (question.correct() === question[key]) {
+                option = (
+                    <button type="submit" ref={correct} className="btn btn-outline-secondary">
+                     
+                        <span className="letter">{key}</span>
+                        <span className="answer">{question[key]}</span>
+                    </button>
+                );
+            } else {
+                option = (
+                    <button type="submit" ref={incorrect} className="btn btn-outline-secondary">
+                        <span className="letter">{key}</span>
+                        <span className="answer">{question[key]}</span>
+                    </button>
+                );
+
+            }
+			options.push(option);
+		}
+		return options;
+	};
+
 	const handleSubmit = event => {
 		event.preventDefault();
 	};
@@ -14,7 +55,8 @@ function App() {
 			<img src={question_svg} alt="" />
 			<h2>Kuala Lumpur is the capital of</h2>
 			<form onSubmit={handleSubmit} className="options">
-				<button type="submit" className="btn btn-outline-secondary">
+                <Options />
+				{/* <button type="submit" className="btn btn-outline-secondary">
 					<span className="letter">A</span>
 					<span className="answer">Vietnam</span>
 				</button>
@@ -29,7 +71,7 @@ function App() {
 				<button type="submit" className="btn btn-outline-secondary">
 					<span className="letter">D</span>
 					<span className="answer">Austria</span>
-				</button>
+				</button> */}
 			</form>
 		</section>
 	);
