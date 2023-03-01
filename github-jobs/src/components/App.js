03966b9c4e999fbfa3016aa00d0f9c8cfac87236
Tochi Davids <tochidavids18@gmail.com>
2023-03-01 21:22:02 +0000

    most sidebar design done

--- github-jobs/src/components/App.js
+++ github-jobs/src/components/App.js
@@ -1,41 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/style.css";
+import Search from "./Search";
+import Sidebar from "./Sidebar";
 
 function App() {
-	const [search, setSearch] = useState();
-
-	const handleSumbit = async event => {
-		event.preventDefault();
-
-		const data = await getData();
-		console.log(data);
-	};
-
-	const getData = async () => {
-		const res = await fetch(
-			`https://serpapi.com/search.json?engine=google_jobs&q=barista&hl=en`,
-		);
-		const data = await res.json();
-		console.log(data);
-		return data;
-	};
-
-	// console.log(getData());
-
 	return (
-		<div className="search-container">
-			<form onSubmit={handleSumbit} className="search-bar">
-				<i className="fa-solid fa-briefcase"></i>
-				<input
-					type="text"
-					placeholder="Title, companies, expertise or benefits"
-					className="search"
-					value={search}
-					onChange={event => setSearch(event.target.value)}
-				/>
-				<input type="submit" value="Search" />
-			</form>
-		</div>
+		<>
+			<Search />
+			<main>
+				<Sidebar />
+			</main>
+		</>
 	);
 }
 
