4f1fac2c0b72554de615522f17336bc43089ed79
Tochi Davids <tochidavids18@gmail.com>
2023-03-12 11:46:34 +0000

    shows inital jobs close to location

--- github-jobs/src/App.jsx
+++ github-jobs/src/App.jsx
@@ -1,37 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/style.css';
 import Search from './components/Search';
 import Sidebar from './components/Sidebar';
 import Jobs from './components/Jobs';
 import JobView from './components/JobView';
+import { getIntial } from '.';
 import { Loading } from 'react-loading-dot';
 
 function App(props) {
-	const { data, loaded } = props;
-	console.log(data);
 	const [jobView, setJobView] = useState(false);
 	const [currentJob, setCurrentJob] = useState();
+	const [data, setData] = useState();
+
+	useEffect(() => {
+		const getData = async () => {
+			const initial = await getIntial();
+			setData(initial);
+			// console.log(initial);
+		};
+
+		getData();
+	}, []);
 
 	if (jobView)
 		return <JobView setJobView={setJobView} currentJob={currentJob} />;
 	return (
 		<>
-			<Search />
-			<main>
-				<Sidebar />
-				{loaded ? (
-					<Jobs
-						itemsPerPage={5}
-						setJobView={setJobView}
-						items={data.jobs_results}
-						setCurrentJob={setCurrentJob}
-					/>
-				) : (
-					<div className="loading">
-						<Loading background="#334680" margin="0.5rem" size="2.5rem" />
-					</div>
-				)}
-			</main>
+			{typeof data === 'undefined' ? (
+				<Loading background="#334680" margin="0.5rem" size="2.5rem" />
+			) : (
+				<>
+					<Search />
+					<main>
+						<Sidebar />
+						<Jobs
+							itemsPerPage={5}
+							setJobView={setJobView}
+							items={data.jobs_results}
+							setCurrentJob={setCurrentJob}
+						/>
+					</main>
+				</>
+			)}
 		</>
 	);
 }
