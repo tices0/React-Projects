028b03bc59c1ca3d110f855373f41baa84f3cffa
Tochi Davids <tochidavids18@gmail.com>
2023-03-15 19:49:20 +0000

    fixed sizing of comments

--- interactive-comment-section/src/components/Replies.jsx
+++ interactive-comment-section/src/components/Replies.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Comments from "./Comment";
 
+// fix reply section design
+
 function Replies({ comment }) {
 	return (
 		<ul className="replies">
 			<div className="line"></div>
-			{comment.replies &&
-				comment.replies.map((reply, index) => (
-					<Comments comment={reply} index={index} />
-				))}
+			<ul className="comments">
+				{comment.replies &&
+					comment.replies.map((reply, index) => (
+						<Comments comment={reply} index={index} />
+					))}
+			</ul>
 		</ul>
 	);
 }
