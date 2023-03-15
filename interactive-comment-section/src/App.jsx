51fe65dfed34a08e607402c1124306a8aed70132
Tochi Davids <tochidavids18@gmail.com>
2023-03-15 20:50:52 +0000

    all components added

--- interactive-comment-section/src/App.jsx
+++ interactive-comment-section/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Comments from "./components/Comment";
-// import { user } from ".";
+import { user } from ".";
 
 // add textarea section at the bottom
 // add functionality to button clicks
 // - reply button causes text area to show
 
 function App({ comments }) {
+	// const [commentValue, setCommentValue] = useState("");
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		// console.log("comment value:", commentValue);
+	};
+
 	return (
 		<>
 			<ul className="comments">
@@ -15,6 +22,20 @@ function App({ comments }) {
 						<Comments comment={comment} index={index} />
 					))}
 			</ul>
+			<form onSubmit={handleSubmit} className="text-box">
+				<img
+					src={require(`./images/avatars/${user.image.png}`)}
+					alt=""
+				/>
+				{/* <textarea
+					name="add-comment"
+					placeholder="Add a comment..."
+					value={commentValue}
+					onChange={event => setCommentValue(event.target.value)}
+				></textarea> */}
+				<div class="textarea" contentEditable></div>
+				<input type="submit" value="SEND" className="submit" />
+			</form>
 		</>
 	);
 }
